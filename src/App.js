72eb78209720e92ce6c20fe336065ab1078d7f0f
OctavioKh <octaviomrq@gmail.com>
2022-08-16 08:54:34 -0500

    Navbar ready, Pre-Firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from"./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+
+
+
+const Shop = () => {
+  return <div>Shoppp</div>;
+};
 
 const App = () => {
-  const categorias = [
-    { id: 1, title: "SNEAKERS", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-    { id: 2, title: "CLOTHING", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-    { id: 3, title: "EYEWEAR", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-    { id: 4, title: "ACCESORIES", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-
-    { id: 5, title: "MEN", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-    { id: 6, title: "WOMEN", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-    // { id: 6, title: "WOMEN", imgSrc: 'https://media.architecturaldigest.com/photos/5dcde0245ff5e60008e76f52/master/w_1600,c_limit/Osofsky_Oct19-571.jpg' },
-  ];
-
   return (
-   <Directory categories={categorias}/>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
